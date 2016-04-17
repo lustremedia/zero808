071edc44c61b37d9b451010cf209e09c4e3d84c6
Tobias <tm@lustremedia.com>
2016-04-18 00:38:12 +0200

    refactoring

--- zero_eight_oh_eight.rb
+++ zero_eight_oh_eight.rb
@@ -1,29 +1,29 @@
 class ZeroEightOhEight
-	attr_reader :time
+	attr_reader :t
 	def input
-		puts 'How bright a time?'
+		puts 'How bright a time? (Format: 23:59 or 2359)'
 		@t = gets.chomp.tr(':','')
 		if @t =~ /([01]\d|2[0-3]):?([0-5]\d)/
 			b = 0
 			@t.each_char { |c| b += lookup(c.to_i) }
-			puts "Brightest you are on the minute!" if b == 26
-			puts "You timeguessing DIM WIT! #{b}" unless b == 26 || b == 8	
-			puts "Thou lovest darkness!"	if b == 8	
+			puts "Brightest, You are on TIME!" if b == 26
+			puts "You time guessing DIMWIT! #{b}" unless b == 26 || b == 8	
+			puts "Thou lovest DARKNESS!"	if b == 8	
 		else
-		  puts "Sorry wrong timezone ..." 
+		  puts "Sorry wrong timezone ... DIMWIT!" 
 		end	
 	end
 
 	def lookup(d)
 		case d
-		when 0,6,9 then	6
 		when 1     then	2
-		when 2,3,5 then	5
-		when 4     then	4
 		when 7     then	3
+		when 4     then	4
+		when 2,3,5 then	5
+		when 0,6,9 then	6	
 		when 8     then	7
 		else
-			puts "What number between 0 and 9 are you testing, DIM WIT?"
+			puts "What number between 0 and 9 are you testing, DIMWIT? Real BRIGHT Darlin'! Real BRIGHT!"
 			0
 		end		
 	end
