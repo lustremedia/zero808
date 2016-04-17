a3571cfddb6442401dbfb04306df6afcd785eaf2
Tobias <tm@lustremedia.com>
2016-04-18 01:29:16 +0200

    shortening implementation: now --> 26 <-- lines of code plus class call

--- zero_eight_oh_eight.rb
+++ zero_eight_oh_eight.rb
@@ -1,15 +1,13 @@
 class ZeroEightOhEight
-	attr_reader :t
 	def input
 		puts 'How bright a time? (Format: 23:59 or 2359)'
 		@t = gets.chomp.tr(':','')
-		if @t =~ /([01]\d|2[0-3])[0-5]\d/ && @t.length < 5
+		if @t =~ /([01]\d|2[0-3])[0-5]\d/ && @t.length == 4
 			b = 0
 			@t.each_char { |c| b += lookup(c.to_i) }
-			
 			puts "Brightest, You are on TIME!" if b == 26
-			puts "You time guessing DIMWIT! #{b}" unless b == 26 || b == 8	
-			puts "Thou lovest DARKNESS!"	if b == 8	
+			puts "You time guessing DIMWIT! #{b}" unless b == 26 || b == 8
+			puts "Thou lovest DARKNESS!"	if b == 8
 		else
 		  puts "Sorry wrong timezone ... DIMWIT!" 
 		end		
@@ -21,10 +19,9 @@ class ZeroEightOhEight
 		when 7     then	3
 		when 4     then	4
 		when 2,3,5 then	5
-		when 0,6,9 then	6	
+		when 0,6,9 then	6
 		when 8     then	7
 		else
-			puts "What number between 0 and 9 are you testing, DIMWIT? Real BRIGHT Darlin'! Real BRIGHT!"
 			0
 		end		
 	end
